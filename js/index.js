00947fe8534d80438e7a9c7cd2940db0d1a73862
Nemo Cheng <tiny.nemo.fish@gmail.com>
2022-02-15 15:10:56 +0800

    loading texturePacker json file successfully

--- js/index.js
+++ js/index.js
@@ -1,17 +1,15 @@
 const app = new PIXI.Application({width:800, height:600,backgroundColor: 0xEDBF98});
 document.body.appendChild(app.view);
 console.log("index.js loaded")
-app.loader
-    .add('p1','./SpriteSheet/Sprite_Sheet.json')
-    .pre((loader)=>{console.log("start loading spritesheet from "+ loader.baseUrl)})
+PIXI.Loader.shared
+    .add('p1', 'Spritesheet/sprite.json')
     .load(onAssetsLoaded);
 
-const boat = PIXI.Sprite.from("Assets/boat.png")
-app.stage.addChild(boat)
+//const boat = PIXI.Sprite.from("Assets/boat.png")
+//app.stage.addChild(boat)
 
 function onAssetsLoaded(loader, res) {
   console.log("spritesheet loading complete")
-  let t = res.p1.textures["boat"]
-  console.log(t)
-  app.stage.addChild(t)
+  console.log(loader, res)
+  // app.stage.addChild(t)
 }
